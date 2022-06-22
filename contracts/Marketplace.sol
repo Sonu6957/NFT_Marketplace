5bfba2e103f8ecefc33b1ed2ae1d75c6edf121d9
Sonu Sagar <41724871+Sonu6957@users.noreply.github.com>
2022-06-22 20:07:14 +0530

    Update Marketplace.sol

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -9,14 +9,17 @@ contract marketplace {
     IERC20 erctoken;
     address contractOwner;
     uint decimals = 10**18;
-    uint royalityInbps;
+    uint royalityInbps;    
+    uint private _listingId = 0 ;
+    mapping (uint => Listing) private _listingmaps;
     
     enum Tokenstatus{
         Active,
         Sold,
         Cancelled
     }
-    struct Listing{
+    
+    struct Listing{                                                          //Details in each listing
         Tokenstatus status;
         address artist;
         address owner;
@@ -24,25 +27,26 @@ contract marketplace {
         uint tokenId;
         uint price;
     }
-    uint private _listingId = 0 ;
-    mapping (uint => Listing) private _listingmaps;
-    constructor(IERC20 token){
+    
+    constructor(IERC20 token){                                              //address of the deployed ERC20 token as an argument
         erctoken = token; 
         contractOwner=msg.sender;       
     }
-    function SetRoyaltyFee(uint royalitypercentage)public{
+    
+    function SetRoyaltyFee(uint royalitypercentage)public{                                                     //Royalty fee setter
         require(msg.sender==contractOwner,"Only owner can change Royalty amount");
         royalityInbps = royalitypercentage*100;
         require(royalitypercentage<=975,"Royalty limit should be less that 97.5");
     }
-    function getRoyaltyFee() public view returns(uint){
+    
+    function getRoyaltyFee() public view returns(uint){                                                         ////Royalty fee getter in bps
         return royalityInbps;
     }
 
-    function TokenListing(address newtoken, uint tokenId, uint price) public payable  {
-        NFTtoken = IERC721(newtoken);
-        NFTtoken.transferFrom(msg.sender,address(this),tokenId);
-        Listing memory listing = Listing(
+    function TokenListing(address newtoken, uint tokenId, uint price) public payable  {                         //To list a token
+        NFTtoken = IERC721(newtoken);                                                                           // arguments:- newtoken ( address of the NFT token)
+        NFTtoken.transferFrom(msg.sender,address(this),tokenId);                                                // tokenId:- tokenID of the token to be listed 
+        Listing memory listing = Listing(                                                                       //price:- selling price
             Tokenstatus.Active,
             msg.sender,
             msg.sender,
@@ -52,36 +56,24 @@ contract marketplace {
         _listingId++;
         _listingmaps[_listingId] = listing;
     }
-    
-    function getListing(uint listingId) public view returns (Listing memory){
-        return _listingmaps[listingId];
-    }
-    function listingprice(uint ld) public view returns(uint){
-        Listing storage listing = _listingmaps[ld];
-        return listing.price;
-    }
+        
     function BuyToken(uint listingId) public payable{
         Listing storage listing = _listingmaps[listingId];
         require(listing.status==Tokenstatus.Active,"The token is not active");
         require(listing.artist!=msg.sender,"Artist cannot be the buyer");
         require(listing.owner!=msg.sender,"Seller cannot be the buyer");
         require(listing.price<=erctoken.balanceOf(msg.sender),"Insufficient balance");
-
-        listing.status = Tokenstatus.Sold;
-        
+        listing.status = Tokenstatus.Sold;        
         uint platformFee = ((listing.price*decimals)/1000)*25;
         uint royaltyfee = ((getRoyaltyFee()*listing.price)/10000);
-        uint finalprice =listing.price-((platformFee/decimals)+royaltyfee);
-        
-
+        uint finalprice =listing.price-((platformFee/decimals)+royaltyfee);    
         NFTtoken.transferFrom(address(this),msg.sender,listing.tokenId);
-
         erctoken.transferFrom(msg.sender,address(this),(platformFee/decimals));
         erctoken.transferFrom(msg.sender,listing.artist,royaltyfee);
         erctoken.transferFrom(msg.sender,listing.owner,finalprice); 
-
         listing.owner = msg.sender;              
     }
+    
     function CancelToken(uint listingId) public{
         Listing storage listing = _listingmaps[listingId];
         require(listing.status == Tokenstatus.Active,"The token is not active");
